83ca3a34cec80413c02931acbb1402d316804451
Karl <karl@test.local>
2012-10-24 10:52:01 +0200

    Übersetzung des bisherigen Installers hinzugefügt und überflüssige german.lang gelöscht Added Translation to the Installer. Deleted german.lang

--- product/install/install.py
+++ product/install/install.py
@@ -1,3 +1,14 @@
-print "Hallo, ich bin das Installscript für die Bibosoftware der Bits und Bytes Computer-AG des Bischöflichen Maria-Montessori-Schulzentrums Leipzig."
-print "Solltest du einen Fehler finden, bitte behalte ihn nicht, sondern melde ihn unter https://github.com/bitsundbytes/bibosoftware/issues. Danke."
-print ""
+#import der module
+import gettext
+import os
+#installation der uebersetzung
+lang = os.environ.get("LANGUAGE", "de_DE")
+trans = gettext.translation("install", "../locale", [lang])
+trans.install()
+#begruessung
+print _("Hello, I am the installscript for the Bibosoftware of the Bits und Bytes Computer-AG of the Bischoefliches Maria-Montessori-Schulzentrums Leipzig.")
+print _("Should you find an Issue please don't keep it report it on https://github.com/bitsundbytes/bibosoftware/issues. Thanks.")
+print _("First I have to ask you a few questions. If you don't know what they mean press enter.")
+#print _("")
+#print _("")
+#print _("")

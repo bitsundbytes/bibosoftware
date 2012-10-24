b5820a948bbcd99d830e52d9eeb3c11bee7a181a
Karl <karl@test.local>
2012-10-24 09:10:47 +0200

    Kurze Begrüßung zum Installer hinzugefügt. Added short greeting to the installer

---
+++ product/install/install.py
@@ -0,0 +1,3 @@
+print "Hallo, ich bin das Installscript für die Bibosoftware der Bits und Bytes Computer-AG des Bischöflichen Maria-Montessori-Schulzentrums Leipzig."
+print "Solltest du einen Fehler finden, bitte behalte ihn nicht, sondern melde ihn unter https://github.com/bitsundbytes/bibosoftware/issues. Danke."
+print ""

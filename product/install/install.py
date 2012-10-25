a84c3c29380cd8ff3607736da272e17aa20a79e3
Karl <karl@test.local>
2012-10-25 17:51:09 +0200

    Online Version von Daten.sqlite gelöscht.(wird automatisch bei der Installation angelegt) Install.py erweitert. Die editierbare install.po hinzugefügt und install.mo erweitert Deleted online version of daten.sqlite. Worked on install.py Added editable install.po and extended install.mo.

--- product/install/install.py
+++ product/install/install.py
@@ -1,6 +1,7 @@
 #import der module
 import gettext
 import os
+import sqlite3
 #installation der uebersetzung
 lang = os.environ.get("LANGUAGE", "de_DE")
 trans = gettext.translation("install", "../locale", [lang])
@@ -9,6 +10,13 @@ trans.install()
 print _("Hello, I am the installscript for the Bibosoftware of the Bits und Bytes Computer-AG of the Bischoefliches Maria-Montessori-Schulzentrums Leipzig.")
 print _("Should you find an Issue please don't keep it report it on https://github.com/bitsundbytes/bibosoftware/issues. Thanks.")
 print _("First I have to ask you a few questions. If you don't know what they mean press enter.")
-#print _("")
-#print _("")
-#print _("")
+#oeffnen der datenbank
+connection = sqlite3.connect("../db/daten.sqlite")
+#initialisiere cursor
+cursor=connection.cursor()
+#erstellen der leeren tabellen
+print _("Creating Table Media")
+cursor.execute("""CREATE TABLE IF NOT EXISTS media (
+    ID INTEGER PRIMARY KEY, signatur INTEGER,
+    systematik VARCHAR, autor VARCHAR, mediatypID INTEGER)""")
+print _("Creating Table User")
